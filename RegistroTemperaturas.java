9576cea286468d97e90152fdecf8e6e0c4f45bc3
Jefferson Galeas <jj.galeass@uea.edu.ec>
2025-09-06 19:23:47 -0500

    Corrección del código

--- RegistroTemperaturas.java
+++ RegistroTemperaturas.java
@@ -9,7 +9,6 @@
  */
 public class RegistroTemperaturas {
     public static void main(String[] args) {
-        // Definir ciudades, días y semanas
         String[] ciudades = {"Ambato", "Quito"};
         String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
         int semanas = 2;
@@ -17,7 +16,6 @@ public class RegistroTemperaturas {
         for (int c = 0; c < ciudades.length; c++) {
             for (int s = 0; s < semanas; s++) {
                 for (int d = 0; d < dias.length; d++) {
-                    // Generar temperatura aleatoria entre 15 y 35 grados
                     temperaturas[c][d][s] = 15 + Math.random() * 20;
                 }
             }
